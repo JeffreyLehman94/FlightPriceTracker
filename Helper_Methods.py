868726c714109412fc72eb17fd3d1df610864691
Jeffrey Lehman <JeffreyLehman94@gmail.com>
2023-02-11 20:35:36 -0500

    Get cheapest flight + print

--- Helper_Methods.py
+++ Helper_Methods.py
@@ -9,10 +9,5 @@ def saveAllData(JSON_TEXT):
     f = open("test", "w")
     f.write(json.dumps(JSON_TEXT,indent=2))
 
-# ctrl + / to block comment
-# example of adding a week to a date below
-# import datetime
-# import datedelta
-# d = datetime.date(2023, 1, 1)
-# d += 1 * datedelta.WEEK
-# print(d)
+def getCheapestFlight(JSON_TEXT):
+    return int(JSON_TEXT['content']['stats']['itineraries']['stops']['direct']['total']['minPrice']['amount'])
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import datedelta
 d = datetime.date(2023, 8, 1)
 d += 1 * datedelta.WEEK
 
-SESSION_ID = getSession_ID(d.year,d.month,d.day)
+SESSION_ID = getSession_ID(d.year, d.month, d.day)
 JSON_DATA = getFlightPrices(SESSION_ID)
 saveAllData(JSON_DATA)
+price = int(getCheapestFlight(JSON_DATA))
+print(price/1000)
